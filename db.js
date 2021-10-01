fdb33f562fbe8799dbf787208dad92f7239b6d8e
Katherine Fancher-Johnes <kljohnes@gmail.com>
2021-09-30 20:58:14 -0400

    added ssl

--- db.js
+++ db.js
@@ -29,6 +29,12 @@ async function syncDb(sequelize, options){
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 module.exports = {sequelize,
